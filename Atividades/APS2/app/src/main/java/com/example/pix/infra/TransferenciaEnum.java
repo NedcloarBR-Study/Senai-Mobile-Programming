6309322d2350085133565fdf3798a484fffd44d2
NedcloarBR <nedcloar1@hotmail.com>
2024-11-14 19:19:22 -0300

    feat: remove APS2

--- Atividades/APS2/app/src/main/java/com/example/pix/infra/TransferenciaEnum.java
+++
@@ -1,6 +0,0 @@
-package com.example.pix.infra;
-
-public enum TransferenciaEnum {
-    PIX,
-    TRANSFERENCIA
-}
--- Atividades/APS2/app/src/main/java/com/example/pix/repositories/Dados.java
+++
@@ -1,7 +0,0 @@
-package com.example.pix.repositories;
-
-import com.example.pix.entities.ContaEntity;
-
-public class Dados {
-    public static ContaEntity conta;
-}
